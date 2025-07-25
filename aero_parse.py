b7da3bbfe63a7e83ad30bd3fae53fc875c016cb7
Aerospaced91 <aerospaceosrs@gmail.com>
2025-07-25 09:47:46 -0700

    What I have so far, man this is tough

--- aero_parse.py
+++ aero_parse.py
@@ -1,9 +1,18 @@
 import argparse
+import sys
+import re
 
 def main():
     
-    args = parse_args()    
-
+    args = parse_args()
+    content = read_file(args.file)
+    lexed_input = json_lexer(content)
+    if json_parser(lexed_input):
+        print("Valid JSON")
+        sys.exit(0)
+    else:
+        print("Invalid JSON")
+        sys.exit(1)
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -14,8 +23,37 @@ def parse_args():
 
     parser.add_argument("file", type=str, nargs="?", help="File to Process")
 
-
     return parser.parse_args()
+
+def read_file(file):
+    if file is None:
+        print("No file specified.")
+        sys.exit(1)
+    with open(file, "r") as f:
+        return f.read()
+    
+def json_lexer(data):
+    token_pattern = r'({|}|:|,|"[^"]*")'
+    lexer = []
+    data = "".join(data.split())
+    tokens = re.findall(token_pattern, data)
+    for token in tokens:
+        if token == "{":
+            lexer.append(("LEFT_BRACE", token))
+        elif token == "}":
+            lexer.append(("RIGHT_BRACE", token))
+        elif token == ":":
+            lexer.append(("COLON", token))
+        elif token.startswith('"') and token.endswith('"'):
+            lexer.append(("STRING", token[1:-1])) #Strip Quotes
+        else:
+            return []
+    return lexer
+
+def json_parser(lexed_json):
+    pass
+        
+    
 
 
 if __name__ == "__main__":
